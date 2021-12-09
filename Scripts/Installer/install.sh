5c1117bebcad667cd59773c23c6e7e8942f6912e
DiasReviews99 <73920660+DiasReviews99@users.noreply.github.com>
2021-12-09 23:36:16 +0600

    Update install.sh

--- Scripts/Installer/install.sh
+++ Scripts/Installer/install.sh
@@ -8,8 +8,8 @@ hash -r
 echo "Installing the System, Please Wait..."
 apt -q install curl -y
 rm /data/data/com.termux/files/usr/etc/bash.bashrc
-wget https://raw.githubusercontent.com/DiasReviews99/DOS-OS/main/Scripts/Bootstrap/bash.bashrc -P /data/data/com.termux/files/usr/etc/
-chmod +x /data/data/com.termux/files/usr/etc/bash.bashrc
-wget https://raw.githubusercontent.com/DiasReviews99/DOS-OS/main/Rootfs/dos-os -P /data/data/com.termux/files/usr/bin
-chmod +x /data/data/com.termux/files/usr/bin/dos-os
+wget https://raw.githubusercontent.com/DiasReviews99/DOS-OS/main/Scripts/Bootstrap/bash.bashrc -P $PREFIX/etc/
+chmod +x $PREFIX/etc/bash.bashrc
+wget https://raw.githubusercontent.com/DiasReviews99/DOS-OS/main/Rootfs/dos-os -P $PREFIX/bin/
+chmod +x $PREFIX/bin/dos-os
 echo "Installed Completed"
