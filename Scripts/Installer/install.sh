e30c3c4f9da4faf41004ed30a79c6c61468053ff
DiasReviews99 <73920660+DiasReviews99@users.noreply.github.com>
2021-12-09 23:19:55 +0600

    Update install.sh

--- Scripts/Installer/install.sh
+++ Scripts/Installer/install.sh
@@ -1,9 +1,12 @@
 echo "Welcome to the DOS-OS Installer"
 echo "Preapring to Installing, Please Wait..."
-apt -q update
-apt -q upgrade
+apt -q update -y
+apt -q upgrade -y
 apt -q install wget -y
 apt -q install proot openssl-tool -y
 hash -r
 echo "Installing the System, Please Wait..."
-apt -q install curl
+apt -q install curl -y
+wget https://raw.githubusercontent.com/DiasReviews99/DOS-OS/main/Scripts/Bootstrap/bash.bashrc /data/data/com.termux/files/usr/etc/
+wget https://raw.githubusercontent.com/DiasReviews99/DOS-OS/main/Rootfs/dos-os /data/data/com.termux/files/usr/bin
+echo "Installed Completed"
